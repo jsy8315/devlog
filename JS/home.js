974279928df7217358a85b50812f04b891a30380
jsy8315 <tdhitbtd@gmail.com>
2024-09-18 19:55:59 +0900

    배경 동영상 일시정지 버튼_배경 멈춤/재생 기능 구현

--- JS/home.js
+++ JS/home.js
@@ -3,6 +3,27 @@
 // 2. 누르면 재생 이미지로 변경, 배경 영상 멈춤
 // 3. 다시 누르면 일시정지 이미지로 변경, 배경 영상 자동 재생, 반복. 
 
-document.getElementsByClassName("bg-video-pause-btn").addEventListener('click', function(){
-    alert('hello')
+var bgVideoBtnCount = 0;
+
+document.getElementsByClassName("bg-video-pause")[0].addEventListener('click', function(e){
+    e.preventDefault();
+
+    bgVideoBtnCount += 1;
+
+    if ( bgVideoBtnCount % 2 == 1 ) {
+        document.getElementsByClassName('bg-video-content')[0].pause();
+        document.getElementsByClassName('bg-video-pause-btn')[0].classList.add('hide-bg-video-pause-btn');
+        document.getElementsByClassName('bg-video-pause-btn')[0].classList.remove('show-bg-video-pause-btn');
+        document.getElementsByClassName('bg-video-play-btn')[0].classList.add('show-bg-video-play-btn');
+        document.getElementsByClassName('bg-video-play-btn')[0].classList.remove('hide-bg-video-play-btn');
+        
+    } else {
+        document.getElementsByClassName('bg-video-content')[0].play();
+        document.getElementsByClassName('bg-video-pause-btn')[0].classList.add('show-bg-video-pause-btn');
+        document.getElementsByClassName('bg-video-pause-btn')[0].classList.remove('hide-bg-video-pause-btn');
+        document.getElementsByClassName('bg-video-play-btn')[0].classList.add('hide-bg-video-play-btn');
+        document.getElementsByClassName('bg-video-play-btn')[0].classList.remove('show-bg-video-play-btn');
+    }
+    
+    console.log(bgVideoBtnCount);
 })
