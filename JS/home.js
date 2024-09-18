6308d117d1f8e0567c5d6f338746320835ec343a
jsy8315 <tdhitbtd@gmail.com>
2024-09-18 17:56:28 +0900

    배경 동영상 일시정지 버튼 추가

---
+++ JS/home.js
@@ -0,0 +1,8 @@
+// 배경 바탕화면 영상 재생 & 일시정지 버튼
+// 1. 시작스타일은 일시정지 이미지, 배경 영상 자동 재생 중
+// 2. 누르면 재생 이미지로 변경, 배경 영상 멈춤
+// 3. 다시 누르면 일시정지 이미지로 변경, 배경 영상 자동 재생, 반복. 
+
+document.getElementsByClassName("bg-video-pause-btn").addEventListener('click', function(){
+    alert('hello')
+})
