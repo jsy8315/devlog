09e94fc9f589b9abf5610f0e40bbcb3223f1019e
jsy8315 <tdhitbtd@gmail.com>
2024-10-17 18:21:55 +0900

    색깔 조정 및 section02 효과 조정01

--- JS/home.js
+++ JS/home.js
@@ -125,6 +125,15 @@ window.addEventListener('scroll', function() {
     }
 })
 
+// aboutme 클릭 시 이동
+document.getElementsByClassName('aboutme')[0].addEventListener('click', function(){
+    window.scrollTo({
+        left: 0,
+        top: 800,
+        behavior: "smooth"
+    })
+})
+
 // Navbar 로고 클릭 시 새로고침
 document.getElementsByClassName('header-logo')[0].addEventListener('click', function(e) {
     history.go();
@@ -197,33 +206,33 @@ document.getElementsByClassName('topBtn')[0].addEventListener('click', function(
 // section02 요소 이동, 300부터 효과 시작 900에 종료
 // aboutmeMain(왼 > 오), aboutmeGithub, aboutmeBlog, aboutmeResume (오 > 왼)
 // 300부터 시작, 800까지 내려오면 완료
-window.addEventListener('scroll', function() {
-    let currentScrollY = this.window.scrollY;
-    // console.log("currentScrollY : " + currentScrollY);
+// window.addEventListener('scroll', function() {
+//     let currentScrollY = this.window.scrollY;
+//     // console.log("currentScrollY : " + currentScrollY);
 
-    let aboutmeMainX = -(1000 / 300) * currentScrollY + 2000;
-    let aboutmeCardsX = -(1000 / 200) * currentScrollY + 4000;
+//     let aboutmeMainX = -(1000 / 300) * currentScrollY + 2000;
+//     let aboutmeCardsX = -(1000 / 200) * currentScrollY + 4000;
     
-    if (currentScrollY <= 300) {
-        // 둘다 안보이게 처리
-        document.getElementsByClassName("aboutmeMain")[0].style.right = 10000 + "px";
-        document.getElementsByClassName("aboutmeCards")[0].style.left = 10000 + "px";
-    } else if ( currentScrollY > 300 && currentScrollY <= 600 ) {
-        document.getElementsByClassName("aboutmeMain")[0].style.right = aboutmeMainX + "px";
-        document.getElementsByClassName("aboutmeCards")[0].style.left = 10000 + "px";
-    } else if ( currentScrollY > 600 && currentScrollY <= 800 ) {
-        // main은 right : 0으로 고정
-        document.getElementsByClassName("aboutmeMain")[0].style.right = 0 + "px";
-        document.getElementsByClassName("aboutmeCards")[0].style.left = aboutmeCardsX + "px";
-    } else  {
-        // 900이상
-        document.getElementsByClassName("aboutmeMain")[0].style.right = 0 + "px";
-        document.getElementsByClassName("aboutmeCards")[0].style.left = 0 + "px";
-    }
+//     if (currentScrollY <= 300) {
+//         // 둘다 안보이게 처리
+//         document.getElementsByClassName("aboutmeMain")[0].style.right = 10000 + "px";
+//         document.getElementsByClassName("aboutmeCards")[0].style.left = 10000 + "px";
+//     } else if ( currentScrollY > 300 && currentScrollY <= 600 ) {
+//         document.getElementsByClassName("aboutmeMain")[0].style.right = aboutmeMainX + "px";
+//         document.getElementsByClassName("aboutmeCards")[0].style.left = 10000 + "px";
+//     } else if ( currentScrollY > 600 && currentScrollY <= 800 ) {
+//         // main은 right : 0으로 고정
+//         document.getElementsByClassName("aboutmeMain")[0].style.right = 0 + "px";
+//         document.getElementsByClassName("aboutmeCards")[0].style.left = aboutmeCardsX + "px";
+//     } else  {
+//         // 900이상
+//         document.getElementsByClassName("aboutmeMain")[0].style.right = 0 + "px";
+//         document.getElementsByClassName("aboutmeCards")[0].style.left = 0 + "px";
+//     }
 
-    // console.log("aboutmeMainX : " + aboutmeMainX);
+//     // console.log("aboutmeMainX : " + aboutmeMainX);
 
-})
+// })
 
 //Section03 스크롤에 따른 카드 뒤집기 효과
 window.addEventListener('scroll', function() {
