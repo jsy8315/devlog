99467bbb6c37ea327947cfa6c6ede1d8d9924a94
jsy8315 <tdhitbtd@gmail.com>
2024-11-13 17:10:13 +0900

    feat(all) : make function clickHandler and playground page

--- JS/devlogHome.js
+++ JS/devlogHome.js
@@ -1,1 +1,19 @@
-console.log(1)
+function clickOpenNewTabHandler(clickTarget, pagelink) {
+    document.getElementsByClassName(`${clickTarget}`)[0].addEventListener('click', function(){
+        window.open(`${pagelink}`);
+    })
+}
+
+function clickMovePageHandler(clickTarget, pagelink){
+    document.getElementsByClassName(`${clickTarget}`)[0].addEventListener('click', function(){
+        window.location.href = `${pagelink}`;
+    })
+}
+
+clickOpenNewTabHandler('navbar-main-aboutme', 'http://jungsuyoung.com');
+
+clickMovePageHandler('navbar-main-playground', '/playground.html')
+
+document.getElementsByClassName('navbar-logo')[0].addEventListener('click', function(){
+    location.reload(true);
+})
