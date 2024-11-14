8fdb23347048a07355d7a912317ced9f76539431
jsy8315 <tdhitbtd@gmail.com>
2024-11-14 18:34:27 +0900

    feat(all) : add fetchPosts for receive data from port 3002 \ using notion api for receive database info and get

--- JS/devlogHome.js
+++ JS/devlogHome.js
@@ -17,3 +17,24 @@ clickMovePageHandler('navbar-main-playground', '/playground.html')
 document.getElementsByClassName('navbar-logo')[0].addEventListener('click', function(){
     location.reload(true);
 })
+
+// 노션에서 받아온 데이터 넘기기
+async function fetchPosts() {
+    const response = await fetch('http://localhost:3002/posts');
+    const posts = await response.json();
+
+    const container = document.getElementsByClassName('main-content-main-list')[0];
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+
+        postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+            <div class="tags">${post.tags.map(tag => `<span>#${tag}</span>`).join(' ')}</div>
+        `;
+        container.appendChild(postElement);
+    });
+}
+
+fetchPosts();
